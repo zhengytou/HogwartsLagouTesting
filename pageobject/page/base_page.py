c9c6c31614fc8fc3cf6103b5b71f868f1597eef6
zyt <zhengyongtao2@simpletour.com>
2020-04-05 23:06:16 +0800

    pageObject

--- pageobject/page/base_page.py
+++ pageobject/page/base_page.py
@@ -5,9 +5,14 @@ from selenium.webdriver.remote.webdriver import WebDriver
 class BasePage:
     _base_url = ""
 
-    def __init__(self, driver: WebDriver):
-        self.driver = driver
-        self.driver.get(self._base_url)
+    def __init__(self, driver: WebDriver = None):
+
+        if driver is None:
+            self.driver.get(self._base_url)
+        else:
+            self.driver = driver
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(3)
 
     def find(self, by, locator):
         return self.driver.find_element(by, locator)
